0d161341e015885b9e6951551c0156a84497dd28
Bent Cardan <bcardan@gmail.com>
2011-12-15 09:31:01 -0500

    starter_files

---
+++ techno/js/myplaylist.js
@@ -0,0 +1,317 @@
+
+var myPlaylist = [
+
+    {
+    
+        mp3:'//s3.amazonaws.com/chairlift/audio/m4a/02OsakaLoopLine.m4a',
+        oga:'//s3.amazonaws.com/chairlift/audio/ogg/02OsakaLoopLine.ogg',
+        title:'Osaka Loop Line',
+        artist:'Discovery',
+        rating:4,
+        buy:'#',
+        price:'0.99',
+        duration:'4:02',
+        cover:'//s3.amazonaws.com/chairlift/audio/artists/discovery.jpg',
+    
+    
+    }, {
+    
+        mp3:'//s3.amazonaws.com/chairlift/audio/m4a/daniel.m4a',
+        oga:'//s3.amazonaws.com/chairlift/audio/ogg/daniel.ogg',
+        title:'Daniel',
+        artist:'Bat for Lashes',
+        rating:3,
+        buy:'#',
+        price:'0.99',
+        duration:'4:12',
+        cover:'//s3.amazonaws.com/chairlift/audio/artists/daniel.jpg',
+    
+    
+    }, {
+        mp3:'//s3.amazonaws.com/chairlift/audio/m4a/paradise.m4a',
+        oga:'//s3.amazonaws.com/chairlift/audio/m4a/paradise.ogg',
+        title:'Paradise',
+        artist:'Coldplay',
+        rating:4,
+        buy:'#',
+        price:'0.99',
+        duration:'4:37',
+        cover:'mix/xyloto.jpg',
+       
+    }, {
+    
+        mp3:'//s3.amazonaws.com/chairlift/audio/m4a/music.m4a',
+        oga:'//s3.amazonaws.com/chairlift/audio/ogg/music.ogg',
+        title:'Music Is My Hot Sex',
+        artist:'CSS',
+        rating:3,
+        buy:'#',
+        price:'0.99',
+        duration:'3:48',
+        cover:'//s3.amazonaws.com/chairlift/audio/artists/css.jpg',
+    
+    
+    }, {
+    
+        mp3:'//s3.amazonaws.com/chairlift/audio/m4a/pumpedupkicks.m4a',
+        oga:'//s3.amazonaws.com/chairlift/audio/m4a/pumpedupkicks.ogg',
+        title:'Pumped Up Kicks',
+        artist:'Foster The People',
+        rating:5,
+        buy:'#',
+        price:'0.99',
+        duration:'4:00',
+        cover:'http://www.thefastertimes.com/pop/files/2011/08/PumpedUpKicks.jpg',
+    
+    
+    }, {
+    
+        mp3:'//s3.amazonaws.com/chairlift/audio/m4a/hustlermusic.m4a',
+        oga:'//s3.amazonaws.com/chairlift/audio/ogg/hustlermusic.ogg',
+        title:'Hustler Musik',
+        artist:'Lil Wayne',
+        rating:5,
+        buy:'#',
+        price:'0.99',
+        duration:'5:04',
+        cover:'//s3.amazonaws.com/chairlift/audio/artists/carterii.jpg',
+    
+    
+    }, {
+    
+        mp3:'//s3.amazonaws.com/chairlift/audio/m4a/littlebit.m4a',
+        oga:'//s3.amazonaws.com/chairlift/audio/ogg/littlebit.ogg',
+        title:'Little Bit',
+        artist:'Lykke Li',
+        rating:5,
+        buy:'#',
+        price:'0.99',
+        duration:'4:33',
+        cover:'//s3.amazonaws.com/chairlift/audio/artists/lykke.jpg',
+    
+    
+    }, {
+    
+        mp3:'//s3.amazonaws.com/chairlift/audio/m4a/animal.m4a',
+        oga:'//s3.amazonaws.com/chairlift/audio/ogg/animal.ogg',
+        title:'Animal (The Punks Jump Up Remix)',
+        artist:'Miike Snow',
+        rating:5,
+        buy:'#',
+        price:'0.99',
+        duration:'3:32',
+        cover:'//s3.amazonaws.com/chairlift/audio/artists/miike_snow-miike_snow_480.jpg',
+    
+    
+    }, {
+    
+        mp3:'//s3.amazonaws.com/chairlift/audio/m4a/boy.m4a',
+        oga:'//s3.amazonaws.com/chairlift/audio/ogg/boy.ogg',
+        title:'And I Was A Boy From School',
+        artist:'Hot Chip',
+        rating:3,
+        buy:'#',
+        price:'0.99',
+        duration:'5:20',
+        cover:'//s3.amazonaws.com/chairlift/audio/artists/the_warning.png',
+    
+    
+    }, {
+    
+        mp3:'//s3.amazonaws.com/chairlift/audio/m4a/cosmic.m4a',
+        oga:'//s3.amazonaws.com/chairlift/audio/ogg/cosmic.ogg',
+        title:'Cosmic Love',
+        artist:'Florence + The Machine',
+        rating:2,
+        buy:'#',
+        price:'0.99',
+        duration:'4:16',
+        cover:'//s3.amazonaws.com/chairlift/audio/artists/florence.jpg',
+    
+    
+    }, {
+    
+        mp3:'//s3.amazonaws.com/chairlift/audio/m4a/cuddlefuddle.m4a',
+        oga:'//s3.amazonaws.com/chairlift/audio/ogg/cuddlefuddle.ogg',
+        title:'Cuddle Fuddle',
+        artist:'Passion Pit',
+        rating:2,
+        buy:'#',
+        price:'0.99',
+        duration:'4:33',
+        cover:'//s3.amazonaws.com/chairlift/audio/artists/passionpit.jpg',
+    
+    
+    }, {
+    
+        mp3:'//s3.amazonaws.com/chairlift/audio/m4a/search.m4a',
+        oga:'//s3.amazonaws.com/chairlift/audio/ogg/search.ogg',
+        title:'In Search Of',
+        artist:'Miike Snow',
+        rating:5,
+        buy:'#',
+        price:'0.99',
+        duration:'5:16',
+        cover:'//s3.amazonaws.com/chairlift/audio/artists/miike_snow-miike_snow_480.jpg',
+    
+    
+    }, {
+    
+        mp3:'//s3.amazonaws.com/chairlift/audio/m4a/bedrock.m4a',
+        oga:'//s3.amazonaws.com/chairlift/audio/ogg/bedrock.ogg',
+        title:'Bed Rock',
+        artist:'Lil Wayne',
+        rating:5,
+        buy:'#',
+        price:'0.99',
+        duration:'2:46',
+        cover:'//s3.amazonaws.com/chairlift/audio/artists/officialwhitelabel.jpg',
+    
+    
+    }, {
+    
+        mp3:'//s3.amazonaws.com/chairlift/audio/m4a/kidsoracular.m4a',
+        oga:'//s3.amazonaws.com/chairlift/audio/ogg/kidsoracular.ogg',
+        title:'Kids Oracular Spectacular',
+        artist:'MGMT',
+        rating:3,
+        buy:'#',
+        price:'0.99',
+        duration:'5:03',
+        cover:'//s3.amazonaws.com/chairlift/audio/artists/oracularspectacular.jpg',
+    
+    
+    }, {
+    
+        mp3:'//s3.amazonaws.com/chairlift/audio/m4a/InMyPlace.m4a',
+        oga:'//s3.amazonaws.com/chairlift/audio/ogg/InMyPlace.ogg',
+        title:'In My Place',
+        artist:'Coldplay',
+        rating:5,
+        buy:'#',
+        price:'0.99',
+        duration:'3:49',
+        cover:'//s3.amazonaws.com/chairlift/audio/artists/inmyplace.jpg',
+    
+    
+    }, {
+    
+        mp3:'//s3.amazonaws.com/chairlift/audio/m4a/hand.m4a',
+        oga:'//s3.amazonaws.com/chairlift/audio/ogg/hand.ogg',
+        title:'Hold YA Hand',
+        artist:'Mac Dre',
+        rating:4,
+        buy:'#',
+        price:'0.99',
+        duration:'1:24',
+        cover:'//s3.amazonaws.com/chairlift/audio/artists/ripmacdre.jpg',
+    
+    
+    }, {
+    
+        mp3:'//s3.amazonaws.com/chairlift/audio/m4a/level.m4a',
+        oga:'//s3.amazonaws.com/chairlift/audio/ogg/level.ogg',
+        title:'On My Level',
+        artist:'Wiz Khalifa',
+        rating:3,
+        buy:'#',
+        price:'0.99',
+        duration:'4:32',
+        cover:'//s3.amazonaws.com/chairlift/audio/artists/whiz.jpg',
+    
+    
+    }, {
+    
+        mp3:'//s3.amazonaws.com/chairlift/audio/m4a/dontstopw.m4a',
+        oga:'//s3.amazonaws.com/chairlift/audio/ogg/dontstopw.ogg',
+        title:'Dont stop, wont stop',
+        artist:'Lil Wayne',
+        rating:5,
+        buy:'#',
+        price:'0.99',
+        duration:'2:31',
+        cover:'http://upload.wikimedia.org/wikipedia/en/1/19/Da_Drought_3_cover.jpg',
+    
+    
+    }, {
+    
+        mp3:'//s3.amazonaws.com/chairlift/audio/m4a/howthegamegoes.m4a',
+        oga:'//s3.amazonaws.com/chairlift/audio/ogg/howthegamegoes.ogg',
+        title:'How The Game Goes',
+        artist:'Mac Dre',
+        rating:5,
+        buy:'#',
+        price:'0.99',
+        duration:'1:43',
+        cover:'http://cdn.images.juno.co.uk/150/CS1328359-02A.jpg',
+    
+    
+    }, {
+    
+        mp3:'//s3.amazonaws.com/chairlift/audio/m4a/alliwanttodo.m4a',
+        oga:'//s3.amazonaws.com/chairlift/audio/ogg/alliwanttodo.ogg',
+        title:'All I Want To Do',
+        artist:'Mac Dre',
+        rating:3,
+        buy:'#',
+        price:'0.99',
+        duration:'4:11',
+        cover:'http://slstatic.songlyricscom.netdna-cdn.com/album_covers/11/mac-dre-thizzel-washington/mac-dre-30101-thizzel-washington.jpg',
+    
+    
+    }, {
+    
+        mp3:'//s3.amazonaws.com/chairlift/audio/m4a/playa.m4a',
+        oga:'//s3.amazonaws.com/chairlift/audio/ogg/playa.ogg',
+        title:'Playa Like Me Ft. Too Short',
+        artist:'Keak da Sneak',
+        rating:4,
+        buy:'#',
+        price:'0.99',
+        duration:'4:24',
+        cover:'//2.bp.blogspot.com/_0oP0FKIemv0/SPymTQu-V4I/AAAAAAAAA6k/g1DJp6wcFKg/s320/Defied.jpg',
+    
+    
+    }, {
+    
+        mp3:'//s3.amazonaws.com/chairlift/audio/m4a/dontstop.m4a',
+        oga:'//s3.amazonaws.com/chairlift/audio/ogg/dontstop.ogg',
+        title:'Dont stop',
+        artist:'2 Pac',
+        rating:3,
+        buy:'#',
+        price:'0.99',
+        duration:'2:31',
+        cover:'http://4.bp.blogspot.com/_srnd67uQyGs/TT6760wIMzI/AAAAAAAAAkQ/DwFm_5-9Opw/s1600/2pacPacs_Life.jpg',
+    
+    
+    }, {
+    
+        mp3:'//s3.amazonaws.com/chairlift/audio/m4a/InternationalFuckAllYall.m4a',
+        oga:'//s3.amazonaws.com/chairlift/audio/ogg/InternationalFuckAllYall.ogg',
+        title:'International--Fuck All Yall',
+        artist:'2 Pac',
+        rating:4,
+        buy:'#',
+        price:'0.99',
+        duration:'4:01',
+        cover:'http://4.bp.blogspot.com/_srnd67uQyGs/TT6760wIMzI/AAAAAAAAAkQ/DwFm_5-9Opw/s1600/2pacPacs_Life.jpg',
+    
+    
+    }, {
+    
+        mp3:'mix/wayback.mp3',
+        oga:'mix/wayback.ogg',
+        title:'Way Back',
+        artist:'Keak da Sneak',
+        rating:5,
+        buy:'#',
+        price:'0.99',
+        duration:'4:42',
+        cover:'//s3.amazonaws.com/chairlift/audio/artists/keak.jpg',
+    
+    
+    } 
+       
+    
+];
